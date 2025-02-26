b4da5246ea4e890463a8e258c5bab1233ebe0932
Dani <danial.nabiyan1382@gmail.com>
2025-02-26 17:43:20 +0330

    add print for test

--- csi_service.py
+++ csi_service.py
@@ -58,7 +58,9 @@ class ControllerService(csi_pb2_grpc.ControllerServicer):
             )
         parameters = request.parameters
         storage_model = parameters.get("storagemodel", "")
+        print(f"storagemodel is {storage_model}")
         device_name = find_disk(storage_model)
+        print(f"device name is {device_name}")
         mount_device(device_name)
         create_img(volume_id=request.name,size=request.capacity_range.required_bytes)
         umount_device(device_name)
