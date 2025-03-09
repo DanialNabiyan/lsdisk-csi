9534f84a32827e711330ac4d0ed91b39e9ee3b12
Dani <danial.nabiyan1382@gmail.com>
2025-03-09 17:48:55 +0330

    add function about create pod

--- utils.py
+++ utils.py
@@ -1,13 +1,11 @@
 from asyncio import sleep
-import json
 import os
 import subprocess
-import uuid
+from time import time
 from pathlib import Path
 
 from munch import Munch
 from kubernetes import client, config
-import yaml
 
 config.load_incluster_config()
 
@@ -37,20 +35,20 @@ def run_daemonset(daemonset_name,selector,container_name,image,storagemodel):
     daemonset_manifest = {
         "apiVersion": "apps/v1",
         "kind": "DaemonSet",
-        "metadata": {"name": f"{daemonset_name}"},
+        "metadata": {"name": daemonset_name},
         "spec": {
-            "selector": {"matchLabels": {"app": f"{selector}"}},
+            "selector": {"matchLabels": {"app": selector}},
             "template": {
-                "metadata": {"labels": {"app": f"{selector}"}},
+                "metadata": {"labels": {"app": selector}},
                 "spec": {
                     "containers": [
                         {
-                            "name": f"{container_name}",
-                            "image": f"{image}",
+                            "name": container_name,
+                            "image": image,
                             "env": [
                                 {
                                     "name": "storagemodel",
-                                    "value": f"{storagemodel}"
+                                    "value": storagemodel
                                 },
                                 {
                                     "name": "NODE_NAME",
@@ -69,16 +67,102 @@ def run_daemonset(daemonset_name,selector,container_name,image,storagemodel):
         },
     }
     api_instance.create_namespaced_daemon_set(namespace="default", body=daemonset_manifest)
-    print("DaemonSet created!")
+    if wait_for_daemonset_ready(daemonset_name):
+        print("DaemonSet is fully up and running!")
+    else:
+        print("DaemonSet failed to reach the desired state.")
+
+def wait_for_daemonset_ready(daemonset_name, namespace="default", timeout=300, interval=5):
+    api_instance = client.AppsV1Api()
     
-def list_pod_by_selector(selector):
+    start_time = time.time()
+    
+    while time.time() - start_time < timeout:
+        daemonset = api_instance.read_namespaced_daemon_set(name=daemonset_name, namespace=namespace)
+        desired = daemonset.status.desired_number_scheduled
+        ready = daemonset.status.number_ready
+
+        print(f"Checking DaemonSet: Desired={desired}, Ready={ready}")
+
+        if desired == ready:
+            return True
+        
+        time.sleep(interval)
+
+    return False
+
+def delete_daemonset(daemonset_name,namespace="default"):
+    api_instance = client.AppsV1Api()
+    api_instance.delete_namespaced_daemon_set(
+            name=daemonset_name,
+            namespace=namespace,
+            body=client.V1DeleteOptions(propagation_policy="Foreground")
+        )
+    
+def run_pod(pod_name,container_name,node_name,image,command,disk_path,size,volume_name,namespace="default"):
+    api = client.CoreV1Api()
+    pod_manifest = {
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {"name": pod_name},
+        "spec": {
+            "nodeName": node_name,
+            "containers": [
+                {
+                    "name": container_name,
+                    "image": image,
+                    "securityContext": {
+                        "privileged": True
+                    },
+                    "command": ["python", command],
+                    "env": [
+                                {
+                                    "name": "disk",
+                                    "value": disk_path
+                                },
+                                {
+                                    "name": "size",
+                                    "value": size
+                                },
+                                {
+                                    "name": "volume",
+                                    "value": volume_name
+                                }
+                           ]
+                }
+            ]
+        }
+    }
+    api.create_namespaced_pod(namespace=namespace, body=pod_manifest)
+    
+def get_pod_status(pod_name,namespace="default"):
     v1 = client.CoreV1Api()
-    pods = v1.list_namespaced_pod(namespace="default", label_selector=selector)
+    pod = v1.read_namespaced_pod(name=pod_name, namespace=namespace)
+    return pod.status.phase
+
+def wait_for_pod_Succeeded(pod_name, namespace="default", timeout=300, interval=5):   
+    start_time = time.time()
+    
+    while time.time() - start_time < timeout:
+        state = get_pod_status(pod_name=pod_name)
+        if state == "Succeeded":
+            return True
+        time.sleep(interval)
+
+    return False
+
+def delete_pod(pod_name,namespace="default"):
+    v1 = client.CoreV1Api()
+    v1.delete_namespaced_pod(name=pod_name, namespace=namespace)
+
+def list_pod_by_selector(selector,namespace="default"):
+    v1 = client.CoreV1Api()
+    pods = v1.list_namespaced_pod(namespace=namespace, label_selector=selector)
     return pods
 
-def get_log_by_podname(pod):
+def get_log_by_podname(pod,namespace="default"):
     v1 = client.CoreV1Api()
-    return v1.read_namespaced_pod_log(name=pod, namespace="default")
+    return v1.read_namespaced_pod_log(name=pod, namespace=namespace)
     
 def get_storageclass_storagemodel_param(storageclass_name):
     api_instance = client.StorageV1Api()
@@ -92,15 +176,6 @@ def get_storageclass_from_pv(pvname):
     pv = api_instance.read_persistent_volume(pvname)
     pv = Munch.fromDict(pv)
     return pv.spec.storage_class_name
-
-def create_symlink(path, to):
-    path = Path(path)
-    to = Path(to)
-    if path.is_symlink():
-        if os.readlink(path) == str(to):
-            return
-    be_absent(path)
-    path.symlink_to(to)
 
 def be_absent(path):
     path = Path(path)
