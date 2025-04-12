90d7eb1968e8e7e5851d9fbc9637355efe62a7e9
Dani <danial.nabiyan1382@gmail.com>
2025-04-12 11:22:26 +0330

    create logger file

---
+++ logger.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+# Configure the logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the default logging level
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),  # Log to the console
+    ],
+)
+
+# Create a logger instance for the application
+def get_logger(name: str) -> logging.Logger:
+    return logging.getLogger(name)
