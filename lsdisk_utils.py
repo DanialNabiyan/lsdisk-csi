4289b34ef720bdaf9f878439491d1c0936f10eac
Dani <danial.nabiyan1382@gmail.com>
2025-04-27 16:44:17 +0330

    fix function find_loop_from_path

--- lsdisk_utils.py
+++ lsdisk_utils.py
@@ -139,7 +139,7 @@ def detach_loops(file) -> None:
         
 def find_loop_from_path(path):
     result = run_out(f"findmnt {path}").stdout.decode()
-    output_lines = result.stdout.splitlines()
+    output_lines = result.splitlines()
     data_line = output_lines[1]
     columns = data_line.split()
     source_value = columns[1]
