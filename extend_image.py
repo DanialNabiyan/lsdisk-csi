4a8419e3eee607a5a2436af0005782a019880397
Dani <danial.nabiyan1382@gmail.com>
2025-05-10 15:36:00 +0330

    change extend_image mountpoint

--- extend_image.py
+++ extend_image.py
@@ -16,10 +16,10 @@ mount_dest = os.getenv("MOUNT_DEST")
 disks = find_disk(storage_model=storagemodel)
 for disk in disks:
     logger.info(f"Found disk: {disk}")
-    mount_device(src=f"/dev/{disk}", dest=f"{mount_dest}/{volume_id}")
+    mount_device(src=f"/dev/{disk}", dest=mount_dest)
     expand = expand_img(volume_id=volume_id, size=int(capacity_range))
     logger.info(f"expand img: {expand}")
-    umount_device(f"{mount_dest}/{volume_id}")
+    umount_device(mount_dest)
     if expand:
         logger.info(f"Image {volume_id} extended successfully")
         break
